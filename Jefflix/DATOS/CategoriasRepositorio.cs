f21f45cc6347a40aa8e60f2beb09be6bb5b7d27e
JEFFREY ALEJANDRO VERDU HERNANDEZ <JEFFREY.VERDU@correoaiep.cl>
2022-08-30 19:06:27 -0400

    tercer commit

---
+++ Jefflix/DATOS/CategoriasRepositorio.cs
@@ -0,0 +1,32 @@
+﻿using Jefflix.NEGOCIO;
+using Microsoft.Data.SqlClient;
+
+namespace Jefflix.DATOS
+{
+    public class CategoriasRepositorio
+    {
+        private readonly IConfiguration _configuration;
+
+        public CategoriasRepositorio(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public List<Categorias> ObtenerTodas()
+        {
+            var listaCategorias = new List<Categorias>();
+            using SqlConnection sql = new SqlConnection(_configuration.GetConnectionString("defaultConnection"));
+            using SqlCommand cmd = new SqlCommand("select * from Categorias",sql);
+            sql.Open();
+            using(var reader = cmd.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    Categorias categoria = new Categorias { Id = (int)reader["Id"], Nombre = reader["Nombre"].ToString() };
+                    listaCategorias.Add(categoria);
+                }
+            }
+
+            return listaCategorias;
+        }
+    }
+}
---
+++ Jefflix/NEGOCIO/Categorias.cs
@@ -0,0 +1,8 @@
+﻿namespace Jefflix.NEGOCIO
+{
+    public class Categorias
+    {
+        public int Id { get; set; } 
+        public string Nombre { get; set; } 
+    }
+}
